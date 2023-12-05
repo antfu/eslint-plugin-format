c7c4d176965331c9b7b1b9739d13f294cbbc6297
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-05 03:01:08 +0100

    fix: patch parser info

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
-import * as parserPlain from 'eslint-parser-plain'
+import * as _parserPlain from 'eslint-parser-plain'
+import type { Linter } from 'eslint'
 import prettier from './rules/prettier'
 import dprint from './rules/dprint'
+
+const parserPlain: Linter.ParserModule = {
+  meta: {
+    name: 'eslint-parser-plain',
+  },
+  ..._parserPlain as { parseForESLint: any },
+}
 
 export default {
   parserPlain,
